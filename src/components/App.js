bb616815d6dfda245698f77d13be77f3afa651cc
Prathamesh-1234 <thispc2803@gmail.com>
2026-05-07 15:31:35 +0530

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,16 @@
-
 import React from "react";
 import './../styles/App.css';
 import PageLayout from "./PageLayout";
 
 const App = () => {
-  let header='Welcome to my website' 
-  let footer='© 2023 My Website.All rights reserved'
+
+  let header = <h1>Welcome to my website</h1>
+  let footer = <div>© 2023 My Website. All rights reserved.</div>
   return (
     <div>
         {/* Do not remove the main div */}
         <PageLayout header={header} footer={footer}>
-        <p>This is the content of my website</p>
+         <p>This is the content of my website.</p>
         </PageLayout>
     </div>
   )
--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -3,11 +3,9 @@ import React from 'react'
 const PageLayout = ({header,footer,children}) => {
   return (
     <div>
-      <h1>{header}</h1>
-      <p>{children}</p>
-      <div>{footer}</div>
-      
-      
+      {header}
+      {children}
+      {footer}
     </div>
   )
 }
